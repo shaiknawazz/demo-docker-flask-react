7133d91d88642eb0f1ee5e5668f71cffb78b77de
shanawa2@in.ibm.com <shanawa2@in.ibm.com>
2021-01-09 18:32:54 +0530

    code restructure

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,16 +7,17 @@ export default function Home(){
         window.location.replace("/customer");
     }
 
-    const handleHotel = () => {
+    const handleVendor = () => {
        window.location.replace("/register")
     }
 
     return (
+
         <div className="bghome">
         <div  style={{ textAlign: "center" ,minWidth: 300, maxWidth: 700,margin: "auto", marginTop: "200px"}}>
-            <h2 style={{color: "white"}}>PROJECT: DINE-IN</h2>
+            <h2 style={{color: "white"}}>QUICK SCAN</h2>
            <button style={{display:"block", width: "100%", backgroundColor:'blue',color:"white", padding:" 15px 32px",fontSize:"16px", pointerEvents: "cursor"}} onClick={() => handleCustomer()}>Customer</button>
-            <button style={{display:"block", width: "100%", padding:" 15px 32px",fontSize:"16px", backgroundColor:'red', color: "white", pointer: "cursor", marginBottom: "20px"}} onClick={() => handleHotel()}>Hotel</button>
+            <button style={{display:"block", width: "100%", padding:" 15px 32px",fontSize:"16px", backgroundColor:'red', color: "white", pointer: "cursor", marginBottom: "20px"}} onClick={() => handleVendor()}>Hotel</button>
         </div>
          
         </div>
